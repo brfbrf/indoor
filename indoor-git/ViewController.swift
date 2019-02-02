dfbc3ffe0995b708949d914e4b458b4d9d279bfe
M1 <m1@MacBook-Air-M1.local>
2019-02-02 17:17:34 +0300

    Initial Commit

---
+++ indoor-git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  indoor-git
+//
+//  Created by M1 on 02/02/2019.
+//  Copyright © 2019 Dmitry Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
